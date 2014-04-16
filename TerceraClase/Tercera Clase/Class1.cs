28880440b1e0fd2cf75ed999880af08a5ce6a49d
CeGisele <cegisele@hotmail.com>
2014-04-16 10:54:11 -0300

    Introduccion a c#

---
+++ TerceraClase/Tercera Clase/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tercera_Clase
+{
+    class Class1
+    {
+        //Ejemplo 3:
+        /*enum Dias{
+            Domingo,
+            Lunes,
+            Martes,
+            Miercoles,
+            Jueves,
+            Viernes,
+            Sabado
+        }*/
+        static void Main(string[] args)
+        {
+            //Ejemplo 3:
+            /* Dias dia = Dias.Domingo;
+            Console.WriteLine(dia);
+            Console.ReadLine();*/
+
+
+            //Ejemplo 4 - Uso de Convert, Parse y TryParse:
+            short x;
+            int y;
+            string z = "10";
+            y = Convert.ToInt32(z);
+            x = short.Parse(y.ToString());
+            Console.WriteLine(short.TryParse(y.ToString(), out x));
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.ReadLine();
+
+            //Ejemplo 5:
+            /*string Mensaje = "Llegas tarde = horno";
+
+            foreach (char caracter in Mensaje)
+            {
+                Console.WriteLine(caracter);
+            }
+            Console.ReadLine();*/
+        }
+    }
+}
---
+++ TerceraClase/Tercera Clase/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tercera_Clase
+{
+    class Program
+    {
+        // static void Main(string[] args)
+        //{
+
+        //Primer ejemplo:
+        /* int Resultado;
+            Console.WriteLine(Sumar(1, 2, out Resultado));
+            Console.WriteLine(Resultado);
+            Console.ReadLine();*/
+
+        //Segundo ejemplo:
+        /*int Num1 = 5;
+        int Num2 = 7;
+        Sumar(Num1, ref Num2);
+        Console.WriteLine(Num2);
+        Console.ReadLine();*/
+        //}
+        //Primer ejemplo:
+        /* static int Sumar(int Num1, int Num2, out int Resultado)
+            {
+                Resultado=Num1+Num2;
+                return Resultado;
+            }*/
+
+
+        //Segundo ejemplo:
+        /*static int Sumar(int Num1, ref int Num2)
+            {
+                Num2 = Num1 + Num2;
+                return 0;
+            }*/
+        //}
+        //}
+    }
+}
