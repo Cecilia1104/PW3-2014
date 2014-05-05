d0d3f4c453a2abfafae636886189f14f7b409ef6
CeGisele <cegisele@hotmail.com>
2014-05-04 21:06:05 -0300

    Modificacion: Ocutamiento de controles en 404

--- EjerciciosPracticos/EjercicioMasterPages/EjercicioMasterPages/404.aspx
+++ EjerciciosPracticos/EjercicioMasterPages/EjercicioMasterPages/404.aspx
@@ -2,6 +2,6 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-Ups, la página que busca no ha sido encontrada.
+<h1>Ups, la página que busca no ha sido encontrada.</h1>
 </asp:Content>
 
--- EjerciciosPracticos/EjercicioMasterPages/EjercicioMasterPages/404.aspx.cs
+++ EjerciciosPracticos/EjercicioMasterPages/EjercicioMasterPages/404.aspx.cs
@@ -11,9 +11,12 @@ namespace EjercicioMasterPages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-           
+
+            TextBox tx = Page.Master.FindControl("txt") as TextBox; //Asi se acceden a las propiedades del control que esta en la master
+            tx.Visible = false; //Lo oculto
+            Button bu = Page.Master.FindControl("btn") as Button;
+            bu.Visible = false;
+
         }
-        
-        
     }
 }
